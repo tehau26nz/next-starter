8508810bc84e1b7839cd53d9b01daffa68b184e3
Tehaunui Shing Soi <92765882+tehau26nz@users.noreply.github.com>
2023-09-27 12:02:25 +1300

    Updating Netlify Identity Widget

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,7 @@ export default function Home() {
       <Head>
         <title>Hello and welcome to my Next.js testing page</title>
         <link rel="icon" href="/favicon.ico" />
+        <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
       </Head>
 
       <main>
@@ -16,7 +17,7 @@ export default function Home() {
           This will be fun :D
         </p>
       </main>
-
+      
       <Footer />
     </div>
   )
