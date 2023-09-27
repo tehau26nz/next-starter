541b336d09854027ac10064f07ca4ccadd2ff141
Tehaunui Shing Soi <92765882+tehau26nz@users.noreply.github.com>
2023-09-27 14:39:35 +1300

    Updating script

--- pages/index.js
+++ pages/index.js
@@ -19,6 +19,17 @@ export default function Home() {
       </main>
       
       <Footer />
+      <script>
+        if (window.netlifyIdentity) {
+          window.netlifyIdentity.on("init", user => {
+            if (!user) {
+              window.netlifyIdentity.on("login", () => {
+                document.location.href = "/admin/";
+              });
+            }
+          })
+        }
+      </script>
     </div>
   )
 }
